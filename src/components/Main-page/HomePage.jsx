ee687c12d542f9515caa018233559826ff3a095a
Ayush Rudani <ayushcrudani@gmail.com>
2022-05-30 23:51:02 +0530

    BackToTop Button Fixed

--- src/components/Main-page/HomePage.jsx
+++ src/components/Main-page/HomePage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {Myinfo} from "../Top-division-components/Top-division-components.jsx";
+import { Myinfo } from "../Top-division-components/Top-division-components.jsx";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import "./about.css";
-import {Logo, LogoSectionAbout} from "../logo-section/logoSection.jsx";
-import {FirstPrize, PrizeHeading} from "../prize tracks/prizes.jsx";
-import {Prizeinfo} from "../../Module/General";
-import {Accordion} from "../FAQ/faq.jsx";
+import { Logo, LogoSectionAbout } from "../logo-section/logoSection.jsx";
+import { FirstPrize, PrizeHeading } from "../prize tracks/prizes.jsx";
+import { Prizeinfo } from "../../Module/General";
+import { Accordion } from "../FAQ/faq.jsx";
 // import {theme} from "../themes_test/theme.jsx";
 // import {Sponsor, SponsorsHead, SponsorUS} from "../Sponsors/sponsors.jsx";
-import {Sponsor, SponsorsHead} from "../Sponsors/sponsors.jsx";
+import { Sponsor, SponsorsHead } from "../Sponsors/sponsors.jsx";
 import Birds from "../Birds/birds.jsx";
 import Footer from "../Footer/footer.jsx";
-import {Member} from "../team/team.jsx";
+import { Member } from "../team/team.jsx";
 import pattern from "./pattern4.png";
 import Media from "../media/media.jsx";
 import ThemeType from "../Themes/theme.jsx";
@@ -90,6 +90,32 @@ function FrequentlyAsked(props) {
 }
 
 export default function HomePage(props) {
+
+  window.onscroll = () => {
+    toggleTopButton();
+    console.log(document.body.scrollHeight);
+    console.log(document.body.scrollTop);
+    console.log("Diff", document.body.scrollHeight - document.body.scrollTop);
+  }
+
+  const scrollToTop = (event) => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  function toggleTopButton() {
+    if ((document.body.scrollHeight - document.body.scrollTop) < 800) {
+      document.getElementById('back-to-up').style.display = 'none';
+    }
+    else if (document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20) {
+      document.getElementById('back-to-up').style.display = 'block';
+    } else {
+      document.getElementById('back-to-up').style.display = 'none';
+    }
+  }
+
+
+
   React.useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://apply.devfolio.co/v2/sdk.js";
@@ -101,7 +127,7 @@ export default function HomePage(props) {
     };
   }, []);
   return (
-    <div id="top" className="Whole_div" style={{backgroundImage: `url(${pattern})`}}>
+    <div id="top" className="Whole_div" style={{ backgroundImage: `url(${pattern})` }}>
       <div className="color_sectiom">
         <Container fluid>
           <Row className="Row info">
@@ -115,7 +141,7 @@ export default function HomePage(props) {
           </Row>
 
 
-{/*          <Row className="mediaInfo">
+          {/*          <Row className="mediaInfo">
             <Col className="" sm={12} lg={12} md={12}>
               <Media />
             </Col>
@@ -134,64 +160,64 @@ export default function HomePage(props) {
         </Row>
 
 
-      {/*Theme Section*/}
+        {/*Theme Section*/}
         <Row className="prizesection non-coding" id="themes">
           <PrizeHeading type="Hackathon Themes" />
-          <ThemeType/>
-        </Row> 
+          <ThemeType />
+        </Row>
 
 
         <Row className="prizesection" id="prizes">
           <PrizeHeading type="Prizes" />
-          </Row> 
-       <div class="row1-container">
-        
-        <div class="box red">
-          <h2>Overall First</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/1st-place-medal-emoji.png" alt="" />
-          <p>First Overall prize will be given to a project that outstands all other submissions.</p>
-        </div>
+        </Row>
+        <div class="row1-container">
 
-        <div class="box box-down cyan">
-          <h2>Overall Second</h2><img className="imgright" src="https://img.icons8.com/emoji/96/000000/2nd-place-medal-emoji.png" alt="" />
-          <p>Second Overall prize will be given to the second best project of the hackathon.</p>
-        </div>
+          <div class="box red">
+            <h2>Overall First</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/1st-place-medal-emoji.png" alt="" />
+            <p>First Overall prize will be given to a project that outstands all other submissions.</p>
+          </div>
 
-        <div class="box box-down blue">
-          <h2>Overall Third</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/3rd-place-medal-emoji.png" alt="" />
-          <p>Third Overall prize will be given to the third best project of the hackathon.</p>
+          <div class="box box-down cyan">
+            <h2>Overall Second</h2><img className="imgright" src="https://img.icons8.com/emoji/96/000000/2nd-place-medal-emoji.png" alt="" />
+            <p>Second Overall prize will be given to the second best project of the hackathon.</p>
+          </div>
+
+          <div class="box box-down blue">
+            <h2>Overall Third</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/3rd-place-medal-emoji.png" alt="" />
+            <p>Third Overall prize will be given to the third best project of the hackathon.</p>
+          </div>
         </div>
-      </div>
-{/*      <div class="row2-container">
+        {/*      <div class="row2-container">
         <div class="box orange">
           <h2>Karma</h2>
           <p>Regularly evaluates our talent to ensure quality</p>
           <img class="imgright" src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
         </div>
       </div>*/}
-      <div class="row1-container margfromsponsor">
-        <div class="box box-down color2">
-          <h2>Best Women Team</h2><img class="imgright" src="https://img.icons8.com/cute-clipart/64/000000/granny-lesbian.png" alt="" />
-          <p>Your project will qualify for this category if your all team members are women.</p>
+        <div class="row1-container margfromsponsor">
+          <div class="box box-down color2">
+            <h2>Best Women Team</h2><img class="imgright" src="https://img.icons8.com/cute-clipart/64/000000/granny-lesbian.png" alt="" />
+            <p>Your project will qualify for this category if your all team members are women.</p>
+          </div>
+
+          <div class="box color1">
+            <h2>Best DDU Team</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/sports-medal-emoji.png" alt="" />
+            <p>Your project will qualify for this category if your all team members are from DDU.</p>
+          </div>
+
+          <div class="box box-down orange">
+            <h2>More prizes</h2><img class="imgright" src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-prizes-circus-flaticons-lineal-color-flat-icons.png" alt="" />
+            <p>More prizes will be revealed soon !</p>
+          </div>
         </div>
 
-        <div class="box color1">
-          <h2>Best DDU Team</h2><img class="imgright" src="https://img.icons8.com/emoji/96/000000/sports-medal-emoji.png" alt="" />
-          <p>Your project will qualify for this category if your all team members are from DDU.</p>
-        </div>
 
-        <div class="box box-down orange">
-          <h2>More prizes</h2><img class="imgright" src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-prizes-circus-flaticons-lineal-color-flat-icons.png" alt="" />
-          <p>More prizes will be revealed soon !</p>
-        </div>
-      </div>
-
-
-      {/*Theme end*/}
+        {/*Theme end*/}
 
         <Birds top="200vh" left="0vh" type="" />
 
         {/* ********Prizes here ***** */}
-{/*        <Row className="prizesection" id="prizes">
+        {/*        <Row className="prizesection" id="prizes">
           <PrizeHeading type="Prizes" /><br/>
           <div className="prize--cards">
           {Prizeinfo.map(PrizeGroup)}
@@ -203,7 +229,7 @@ export default function HomePage(props) {
         {/* ********Sponsors here ***** */}
 
         <Row className="sponsorSection" id="sponsors">
-         <h1 className="margfromsponsor1">Sponsors & Partners</h1>
+          <h1 className="margfromsponsor1">Sponsors & Partners</h1>
           {/* <SponsorUS /> */}
           {sponsorLogos.map(SponsorGroup)}
         </Row>
@@ -228,17 +254,20 @@ export default function HomePage(props) {
         {/* ********Team ending here ***** */}
 
         {/* ********Team ending here ***** */}
-        <br/><br/>
-          <h1 id="faqs">FAQs</h1>
-          {/* ********Frequently asked Questions here ***** */}
+        <br /><br />
+        <h1 id="faqs">FAQs</h1>
+        {/* ********Frequently asked Questions here ***** */}
         <div className="Myfaqs">
           {frequentlyAskedQuestions.map(FrequentlyAsked)}
           {/* ********Frequently asked Questions ending here ***** */}
         </div>
       </Container>
-      
+
       <Footer />
-      <div class="backtotop"><a href="#top"><p><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAtklEQVRIie3XsQrCMBSF4T+CoJsddHAo9HH6AD634NbNqTgo6FI6WQdTtJLQpG0CxXsglIRyv0ugCYWZRsUGd8ARKIA0FroFTkCjxxnIYqNRcBsaFO9Dg+Cu6KS4LzoJrnh/Mr5oOwpgaSu+GNqVY6wHTN/Jo4DNz1oJrAzv7oH6a/4Ani7duabCvLVrnyKht1pggQUWWOA/g0vD2o3ulRgkOXDhcx1egYNvkTG/IIl+3nUD88gLIHVaGvUECUcAAAAASUVORK5CYII="/></p></a></div>
+      <div class="backtotop" id="back-to-up"><a onClick={scrollToTop}><p><i class="fa-solid fa-arrow-up"></i></p></a></div>
     </div>
   );
+
+
+
 }
