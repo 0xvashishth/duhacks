4ee6b8f7ddd4dd95c665ff7064602f52b02056d4
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-06-10 20:45:21 +0530

    link updated

--- src/components/Top-division-components/Top-division-components.jsx
+++ src/components/Top-division-components/Top-division-components.jsx
@@ -72,7 +72,7 @@ function Myinfo() {
         </button>
       </a>
       <br /><br /><br />
-      <a href="https://org.devfolio.co/duhacks" target="_blank"><button>Learn More</button></a>
+      <a href="https://duhacks.devfolio.co/" target="_blank"><button>Learn More</button></a>
     </div>
 
 
