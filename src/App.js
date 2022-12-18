8bde29cd2b6087f1be872edc5bc544b93ac52417
Prince <prince@gmail.com>
2022-12-18 14:57:06 +0530

    video player added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import About from "./components/About";
-import BackToTop from "./components/BackToTop";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
-import Counter from "./components/Counter";
-import FAQS from "./components/FAQS";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Prize from "./components/Prize";
-import Query from "./components/Query/Query";
-import Schedule from "./components/Schedule";
-import Sponsers from "./components/Sponsers";
-import Video from "./components/Video";
+import About from './components/About';
+import BackToTop from './components/BackToTop';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Counter from './components/Counter';
+import FAQS from './components/FAQS';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Info from './components/Info';
+import Prize from './components/Prize';
+import Query from './components/Query/Query';
+import Schedule from './components/Schedule';
+import Sponsers from './components/Sponsers';
+import Video from './components/Video';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Header />
       <About />
       <Video />
+      <Info />
       <Prize />
       <Counter />
       <Schedule />
